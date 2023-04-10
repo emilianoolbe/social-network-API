e2c7096d8ec5edf635ff1302f2112d71d50a14bd
emilianoolbe <emilianoolbe@gmail.com>
2023-04-10 15:31:15 -0300

    1

--- src/Middlewares/auth.js
+++ src/Middlewares/auth.js
@@ -1,51 +1,4 @@
 // --> JWT + Moment + Clave secreta <--
-<<<<<<< HEAD
-const jwt = require('jwt-simple');
-const moment = require('moment');
-const {secret} = require('../services/jwt');
-
-
-//Función de auntentificación
-const auth = (req, res, next) => {
-    
-    //Comprobar si llega la cabecera de auth -> El valor de req.headers.authotizaation es el token <-
-    const  cabecera = req.headers.authorization;
-
-    if (!cabecera) {
-        return res.status(403).json({
-            status: 'Error',
-            message: 'La petición no tiene la cabecera de autentificación'
-        });
-    };
-
-    //Limpiar token -> puede venir con comillas <-
-    const token = cabecera.replace(/['"] +/g, '');
-
-    //Decodificar el token
-    try {
-        let payload = jwt.decode(token, secret);
-
-        //Comprobar expiración
-        if (payload.exp <= moment().unix()) {
-            return res.status(401).json({
-                status: 'Error',
-                message: 'El token ha expirado'
-            });
-        };
-
-        //Agregar datos de usario a request
-        req.user = payload;
-
-    } catch (error) {
-        return res.status(404).json({
-            status: 'Error',
-            message: 'Token inválido'
-        })
-    };
-    
-    //NEXT()
-    next();
-=======
 const jwt = require("jwt-simple");
 const { secret } = require("../services/jwt");
 const moment = require("moment");
@@ -91,7 +44,6 @@ const auth = (req, res, next) => {
   }
 
   next();
->>>>>>> 8fbcdbec78a867cd1a85d555c070a0855f7c21e2
 };
 
 module.exports = auth;
--- src/services/jwt.js
+++ src/services/jwt.js
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-// --> Dependencias <--
-const jwt = require('jwt-simple');
-const moment = require('moment');
-
-//Clave secreta
-const secret = 'PalabraSecreta_social_Network_6350Vishera';
-
-//Función generadora de tokens
-const createToken = (user) => {
-
-    // Payload es el objeto que va a guardar datos del usuario
-    const payload = {
-        id: user._id,
-        name: user.name,
-        surname: user.surname,
-        nick: user.nick,
-        email: user.email,
-        role: user.role,
-        image: user.image,
-        iat: moment().unix(),
-        exp: moment().add(30, 'days').unix()
-    };
-    
-    //JWT token codificado
-    return jwt.encode(payload, secret);
-};
-
-module.exports = {createToken, secret};
-
-//29242152
-=======
 // --> JWT + Moment <--
 const jwt = require('jwt-simple');
 const moment = require('moment');
@@ -59,4 +27,4 @@ const createToken = (user) => {
 };
 
 module.exports = {createToken, secret};
->>>>>>> 8fbcdbec78a867cd1a85d555c070a0855f7c21e2
+ 
