0b1e61ff44b4f900685203b7ec97ab47fc9556a5
emilianoolbe <emilianoolbe@gmail.com>
2023-04-26 20:56:43 -0300

    db creada, createUser, login, Token, editUser

--- src/Middlewares/guest.js
+++ src/Middlewares/guest.js
@@ -1,12 +1,11 @@
 const guest = (req, res, next) => {
-    console.log(req.user);
-    if (req.user) {
-        return res.status.json({
-            status: 'Error',
-            message: 'Usuario ya logueado'
-        });
-    }
-    next();
+  if (req.user) {
+    return res.status.json({
+      status: "Error",
+      message: "Usuario ya logueado",
+    });
+  }
+  next();
 };
 
 module.exports = guest;
